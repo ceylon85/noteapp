05ef2ba957a498bd6ea80ef74fada3d0dd520017
ceylon85 <canleez123@gmail.com>
2022-03-08 23:13:31 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Note } from './models/note.model'
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([{
+    id: (new Date).toString(),
+    title: 'Meeting',
+    text: 'Shedule Meeting',
+    color: '#dfdfdf',
+    date: (new Date).toString(),
+  }])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface IHeaderProps {
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+  return (
+     
+  );
+};
+
+export default Header;
